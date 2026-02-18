4721d34685e680073d51bc2fd94c7ae741303b1b
rkxrakib <rakibhassanfb2@gmail.com>
2026-02-18 13:32:33 +0600

    Add Firebase configuration to config.php
    Added Firebase configuration settings for the project.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Firebase Configuration Wrapper
+$firebaseConfig = '
+{
+  apiKey: "AIzaSyDvtZJhIN850tU7cETuiqRyCyjCBdlFt-Y",
+  authDomain: "fynora-81313.firebaseapp.com",
+  databaseURL: "https://fynora-81313-default-rtdb.firebaseio.com",
+  projectId: "fynora-81313",
+  storageBucket: "fynora-81313.firebasestorage.app",
+  messagingSenderId: "593306264446",
+  appId: "1:593306264446:web:da476d4c77ae4ede6b492f"
+}';
+?>
